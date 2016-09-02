7e2a34e73bf2898bd2a6e187cc591c9fda62fe9e
David Middlecamp <david@spark.io>
2016-09-02 10:30:05 -0500

    chillax

--- app.js
+++ app.js
@@ -15,14 +15,14 @@ var app = express();
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 //app.use(cookieParser());
-//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 
